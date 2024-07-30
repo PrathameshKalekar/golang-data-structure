f4c3e2b6de655f9fa55925f77fb9d7bbc05e48e6
Prathamesh Kalekar <kalekarprathamesh19@gmail.com>
2024-07-30 16:11:58 +0530

    Array practice remove duplicate from sorted array 2

---
+++ 0Practice-question/arrays/00not-important/remove-duplicate-from-sorted-array-2/RemoveFromSortedArray2.go
@@ -0,0 +1,68 @@
+/*
+80 Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4}
+	answer := removeDuplicatesFromSortedArray2(arr)
+	fmt.Println(answer)
+	fmt.Println(arr)
+}
+
+func removeDuplicatesFromSortedArray2(arr []int) int {
+	if len(arr) <= 2 {
+		return len(arr)
+	}
+
+	pointer := 2
+	//for each element check the second left index that same means the element is duplicated 3 rd time in the sorted array
+	for _, value := range arr[2:] {
+		//if not duplicated add in to the pointer index
+		if value != arr[pointer-2] {
+			arr[pointer] = value
+			pointer++
+		}
+	}
+	//return pointer
+	return pointer
+}
