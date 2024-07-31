3622677a6915f93a9b2a090da51c7faa38365b0d
Prathamesh Kalekar <kalekarprathamesh19@gmail.com>
2024-07-31 11:36:28 +0530

    Hashmap pratice jewels and stones

---
+++ 0Practice-question/hashmap-and-sets/jewels-and-stones/JewelsAndStones.go
@@ -0,0 +1,38 @@
+/*
+771 You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+*/
+package main
+
+import "fmt"
+
+func main() {
+	jewels := "aA"
+	stones := "aAAbbb"
+	answer := jewelsAndStones(jewels, stones)
+	fmt.Println(answer)
+}
+
+func jewelsAndStones(jewels, stones string) int {
+	count := 0
+	jewelsMap := make(map[rune]int)
+	for i, val := range jewels {
+		jewelsMap[val] = i
+	}
+	for _, val := range stones {
+		if _, found := jewelsMap[val]; found {
+			count++
+		}
+	}
+	return count
+}
