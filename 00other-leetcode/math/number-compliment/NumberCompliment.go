a2a4dbf4c8ffd991f2f15dba9dbec762720746d5
Prathamesh Kalekar <kalekarprathamesh19@gmail.com>
2024-08-22 09:26:15 +0530

    Math practice number compliment

---
+++ 00other-leetcode/math/number-compliment/NumberCompliment.go
@@ -0,0 +1,43 @@
+/*
+476 The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer num, return its complement.
+
+Example 1:
+
+Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+Example 2:
+
+Input: num = 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func findComplement(num int) int {
+	// Step 1: Find the bit length of the number
+	bitLength := 0
+	n := num
+	for n > 0 {
+		bitLength++
+		n >>= 1
+	}
+
+	// Step 2: Create a mask with all bits set to 1 of the same length
+	mask := (1 << bitLength) - 1
+
+	// Step 3: XOR the number with the mask to get the complement
+	return num ^ mask
+}
+
+func main() {
+	num := 5
+	fmt.Println("Complement of", num, "is", findComplement(num))
+}
